15212c921ccc16505358fbdff4498ada86c5bd8d
Maciej <maclis124@gmail.com>
2023-11-05 13:30:12 +0100

    Define a view for creatin the User object.

--- tutoringApp/profiles/urls.py
+++ tutoringApp/profiles/urls.py
@@ -6,4 +6,5 @@ app_name = "profiles"
 urlpatterns = [
     path("login/", view=views.LoginView.as_view(), name="login"),
     path("logout/", view=views.logout_view, name="logout"),
+    path("register/", view=views.RegisterView.as_view(), name="register"),
 ]
--- tutoringApp/profiles/views/__init__.py
+++ tutoringApp/profiles/views/__init__.py
@@ -1,6 +1,3 @@
-from .authentication import LoginView, logout_view
+from .authentication import LoginView, RegisterView, logout_view
 
-__all__ = [
-    "LoginView",
-    "logout_view"
-]
+__all__ = ["LoginView", "logout_view", "RegisterView"]
