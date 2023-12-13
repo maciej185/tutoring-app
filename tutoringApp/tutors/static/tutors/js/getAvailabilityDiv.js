f1a5296a7f04c59ed3940c98523303ac7b1c8552
Maciej <maclis124@gmail.com>
2023-12-13 13:39:56 +0100

    Create a JS mechanims responsible for deleting existing availability objects.

--- tutoringApp/tutors/static/tutors/js/getAvailabilityDiv.js
+++ tutoringApp/tutors/static/tutors/js/getAvailabilityDiv.js
@@ -1,4 +1,4 @@
-function getAvailabilityDiv(time, object_id) {
+function getAvailabilityDiv(time, object_id, deleteListener) {
     const availabilityDiv = document.createElement("div")
     availabilityDiv.classList.add("popup-main-availabilites-availability", "popup-main-form", "box", "box-main" ,"adjacent-container")
 
@@ -30,12 +30,14 @@ function getAvailabilityDiv(time, object_id) {
     availabilityInfoDiv.append(availabilityInfoStartDiv, availabilityInfoEndDiv)
 
     const availabilityDeleteDiv = document.createElement("div")
-    availabilityDeleteDiv.classList.add("popup-main-form-delete", "icon_green")
+    availabilityDeleteDiv.classList.add("popup-main-availabilites-availability-delete","popup-main-form-delete", "icon_green")
+    availabilityDeleteDiv.id = `popup-main-availabilites-availability-delete-${object_id}`
     availabilityDeleteDiv.innerHTML = `
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16">
                                     <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z"/>
                                 </svg>
     `
+    availabilityDeleteDiv.addEventListener("click", deleteListener)
     
     availabilityDiv.append(availabilityInfoDiv, availabilityDeleteDiv)
 
