d3e8e5c7762620d92a78019c05c421d936b322a2
Maciej <maclis124@gmail.com>
2023-12-13 15:10:59 +0100

    Create a JS mechanism that automatically sets the end value based on the time input in the start and sessions length.

---
+++ tutoringApp/tutors/static/tutors/js/EndTimeSetter.js
@@ -0,0 +1,33 @@
+class EndTimeSetter {
+    constructor() {
+        this.sessionsDuration = Number(document.querySelector("div#info-duration").innerHTML)
+
+        this.popupMainDivs = Array.from(document.querySelectorAll("div.popup-main"))
+        this.addAvailabilityForms = this.popupMainDivs.map(popupMainDiv => popupMainDiv.querySelector("div.popup-main-availabilites-availability_form"))
+        this.addAvailabilityStartInputs = this.addAvailabilityForms.map(addAvailabilityForm => addAvailabilityForm.querySelector("input.time-input"))
+        this.addAvailabilityEndInputs = this.addAvailabilityForms.map(addAvailabilityForm => addAvailabilityForm.querySelector("input.time-input_readonly"))
+
+        this.addAvailabilityStartInputs.forEach(addAvailabilityStartInput => addAvailabilityStartInput.addEventListener("input", this.startInputInputListener.bind(this)))
+
+    }
+
+    startInputInputListener(e) {
+        if (e.currentTarget.value == "") return
+
+        const inputIndex = e.currentTarget.id.split("-").at(-1)
+
+        const date = new Date(`1970-01-01 ${e.currentTarget.value}`)
+        const newDate = new Date(date.getTime() + this.sessionsDuration*60000)
+
+        const newTime = new Intl.DateTimeFormat(undefined, {
+            hour: '2-digit',
+            minute: '2-digit',
+            hourCycle: "h24"
+          }).format(newDate)
+
+        this.addAvailabilityEndInputs[inputIndex].value = newTime
+    }
+
+}
+
+const end_time_setter = new EndTimeSetter()
