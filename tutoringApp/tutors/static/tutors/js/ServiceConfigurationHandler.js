01c719b5e601d074e58e7cf52fa6d7025344b36f
Maciej <maclis124@gmail.com>
2023-12-16 16:31:38 +0100

    Define JS mechanism for managing forms on the Service configuration page.

---
+++ tutoringApp/tutors/static/tutors/js/ServiceConfigurationHandler.js
@@ -0,0 +1,10 @@
+const service_formset_handler = new FormsetHandler("services-main-list-management_form",
+                                                    "services-main-list",
+                                                    "services-main-list-service_empty-0",
+                                                    "services-main-list-service-delete-0", 
+                                                    "services-main-list-service_empty",
+                                                    "services-main-list-service_empty-delete",
+                                                    "services-main-list-add",
+                                                    "services-main-list-service",
+                                                    "services-main-list-service-delete"
+)
