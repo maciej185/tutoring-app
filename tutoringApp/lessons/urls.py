c9e623cba89ab21ced6a0953595ec040296c55fe
Maciej <maclis124@gmail.com>
2023-12-21 12:20:06 +0100

    Define views for creating Booking and Lesson objects.

--- tutoringApp/lessons/urls.py
+++ tutoringApp/lessons/urls.py
@@ -1,4 +1,12 @@
 from django.urls import path
 
+from lessons import views
+
 app_name = "lessons"
-urlpatterns = []
+urlpatterns = [
+    path(
+        "booking/create/<int:availability_id>",
+        view=views.create_booking_view,
+        name="booking_create",
+    ),
+]
---
+++ tutoringApp/lessons/views/__init__.py
@@ -0,0 +1,3 @@
+from .booking import create_booking_view
+
+__all__ = ["create_booking_view"]
