fbdc69ab1d50c7e98839129b29b5149c5b6e3e86
Maciej <maclis124@gmail.com>
2023-12-26 10:35:41 +0100

    Define a view for displaying Booking objects from Tutors perspective.

--- tutoringApp/lessons/views/__init__.py
+++ tutoringApp/lessons/views/__init__.py
@@ -1,4 +1,4 @@
 from .booking_create import create_booking_view
-from .booking_display import BookingsDisplay4Student
+from .booking_display import BookingsDisplay4Student, BookingsDisplay4Tutor
 
-__all__ = ["create_booking_view", "BookingsDisplay4Student"]
+__all__ = ["create_booking_view", "BookingsDisplay4Student", "BookingsDisplay4Tutor"]
