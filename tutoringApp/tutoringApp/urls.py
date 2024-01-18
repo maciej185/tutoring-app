4bd43a53635268df41d4364b4096198b0dd187c4
Maciej <maclis124@gmail.com>
2024-01-18 08:53:07 +0100

    Create a separate app for the subscription funcionality.

--- tutoringApp/tutoringApp/urls.py
+++ tutoringApp/tutoringApp/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
     path("profiles/", include("profiles.urls")),
     path("tutors/", include("tutors.urls")),
     path("lessons/", include("lessons.urls")),
+    path("subscriptions/", include("subscriptions.urls"))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
