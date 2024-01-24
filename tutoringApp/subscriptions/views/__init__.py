d15e1c9ba9e286af9382b6926a7e9096796367ea
Maciej <maclis124@gmail.com>
2024-01-24 08:05:31 +0100

    Define a view for the `learning_tutor` page.

--- tutoringApp/subscriptions/views/__init__.py
+++ tutoringApp/subscriptions/views/__init__.py
@@ -1,5 +1,8 @@
 from .subscriptions import CreateSubscriptionView
+from .learning_tutor import create_appointment_view, LearningTutorView
 
 __all__ = [
     "CreateSubscriptionView",
+    "LearningTutorView",
+    "create_appointment_view",
 ]
